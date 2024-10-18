0150fdb791246ea24f46ee8b8cadb88029cddca0
Redemption19 <abbanb2@gmail.com>
2024-10-18 22:46:48 +0000

    social-weave-ai-starter

---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Link from "next/link"
+import {
+    SignInButton,
+    SignUpButton,
+    UserButton,
+    SignedOut,
+    SignedIn,
+    useAuth,
+  } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+
+  export function Navbar() {
+    const {userId} = useAuth()
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isScrolled,  setIsScrolled] = useState(false)
+
+    useEffect (() =>{
+        const handleScroll = () => {   
+            setIsScrolled(window.scrollY > 10)
+        };
+    });
+
+    return <div></div>
+  }
